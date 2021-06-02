b0694bd2f6e7a702d06ddd84bc9403e86ae3f380
Hunter <79298881+IsThisUsernameTaken1@users.noreply.github.com>
2021-06-02 08:54:39 -0400

    initial add, used ng forms and tsc for functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, NgModel} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'untitled';
+  title = 'Discount Calculator';
+
+ finalPrice = new FormControl('');
+
+  calculate(initialPrice: number, percentOff: number) {
+    if (initialPrice <= 0 || percentOff <= 0 || percentOff > 100) {
+      this.finalPrice.setValue("Fill in valid numbers")
+    } else {
+      this.finalPrice.setValue("$" +(initialPrice * (1 - (percentOff * 0.01))).toFixed(2).toString());
+    }
+  }
 }
